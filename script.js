b1c2991beaae1598eca6c7443f1227749ffcf93c
OGWhiskers <bayandadlamini02@gmail.com>
2023-08-03 19:39:55 +0200

    Add basic reset button

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const domInitialize = (function(){
     const container = document.querySelector(".container")
+    const reset = document.querySelector(".reset")
 
     const createDiv = (n)=>{
         let num = n*n;
 
         for(let i = 0; i<num;i++){
             const div = document.createElement("div");
+
             div.classList.add("box")
+            div.addEventListener("mouseenter",()=>{
+                div.classList.add("hover")
+            })
             container.appendChild(div);
         }
     }
     createDiv(16);
-    return {container,createDiv}
+    return {container,createDiv,reset}
 })();
 
+domInitialize.reset.addEventListener("click",()=>{
+    const divRemove = document.querySelectorAll(".box")
+    divRemove.forEach((item)=>{
+        return item.classList.remove("hover")
+    })
+})

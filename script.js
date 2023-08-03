6950fbe5112b3f408471ee64321515dccc6b2708
OGWhiskers <bayandadlamini02@gmail.com>
2023-08-03 20:12:37 +0200

    Add grid divs dynamically feature

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 const domInitialize = (function(){
-    const container = document.querySelector(".container")
-    const reset = document.querySelector(".reset")
+
+})();
+
+const createGrid = (function(){
+    const choice = prompt("Choice size of grid?");
+    const container = document.querySelector(".container");
+
+    container.style.gridTemplateColumns = `repeat(${choice},1fr)`;
+
+    container.style.gridTemplateRows = `repeat(${choice},1fr`;
 
     const createDiv = (n)=>{
-        let num = n*n;
+        let num = n * n;
 
-        for(let i = 0; i<num;i++){
+        for(let i = 0; i < num; i++){
             const div = document.createElement("div");
-
+ 
             div.classList.add("box")
             div.addEventListener("mouseenter",()=>{
                 div.classList.add("hover")
             })
+            div.addEventListener("mouseleave",()=>{
+                div.classList.remove("hover")
+            })
             container.appendChild(div);
         }
     }
-    createDiv(16);
-    return {container,createDiv,reset}
-})();
 
-domInitialize.reset.addEventListener("click",()=>{
-    const divRemove = document.querySelectorAll(".box")
-    divRemove.forEach((item)=>{
-        return item.classList.remove("hover")
+    createDiv(choice);
+})()
+
+const resetGrid = (function (){
+    const reset = document.querySelector(".reset");
+
+    reset.addEventListener("click",()=>{
+        const divRemove = document.querySelectorAll(".box")
+        divRemove.forEach((item)=>{
+            return item.classList.remove("hover")
+        })
     })
-})
+
+})()

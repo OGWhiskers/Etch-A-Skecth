0b8b93d48512a385cc21cd7e5bee6b95235cf460
OGWhiskers <bayandadlamini02@gmail.com>
2023-08-05 01:19:07 +0200

    Add feature where you can choose grid size

--- script.js
+++ script.js
@@ -1,43 +1,38 @@
-const domInitialize = (function(){
+const createGrid = (function () {
+  const container = document.querySelector(".container");
+  const choiceBtn = document.querySelector(".choice");
 
-})();
+  choiceBtn.addEventListener("click", () => {
+    let choice = prompt("Choice size of grid?");
 
-const createGrid = (function(){
-    const choice = prompt("Choice size of grid?");
-    const container = document.querySelector(".container");
+    const createDiv = (n) => {
+      let num = n * n;
 
-    container.style.gridTemplateColumns = `repeat(${choice},1fr)`;
+      container.style.gridTemplateColumns = `repeat(${choice},1fr)`;
 
-    container.style.gridTemplateRows = `repeat(${choice},1fr`;
+      container.style.gridTemplateRows = `repeat(${choice},1fr`;
+      for (let i = 0; i < num; i++) {
+        const div = document.createElement("div");
 
-    const createDiv = (n)=>{
-        let num = n * n;
-
-        for(let i = 0; i < num; i++){
-            const div = document.createElement("div");
- 
-            div.classList.add("box")
-            div.addEventListener("mouseenter",()=>{
-                div.classList.add("hover")
-            })
-            div.addEventListener("mouseleave",()=>{
-                div.classList.remove("hover")
-            })
-            container.appendChild(div);
-        }
-    }
+        div.classList.add("box");
+        div.addEventListener("mouseenter", () => {
+          div.classList.add("hover");
+        });
+        container.appendChild(div);
+      }
+    };
 
     createDiv(choice);
-})()
+  });
+})();
 
-const resetGrid = (function (){
-    const reset = document.querySelector(".reset");
+const resetGrid = (function () {
+  const reset = document.querySelector(".reset");
 
-    reset.addEventListener("click",()=>{
-        const divRemove = document.querySelectorAll(".box")
-        divRemove.forEach((item)=>{
-            return item.classList.remove("hover")
-        })
-    })
-
-})()
+  reset.addEventListener("click", () => {
+    const divRemove = document.querySelectorAll(".box");
+    divRemove.forEach((item) => {
+      return item.classList.remove("hover");
+    });
+  });
+})();
